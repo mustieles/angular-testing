9ddbeb84def461689b008cd2b6bcb85eea553649
Eric Mustieles <eric.mustieles@redcodeagency.com>
2016-01-04 17:32:20 +0100

    Adds routing and comments.

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.config([
         url: '/home',
         templateUrl: '/home.html',
         controller: 'MainCtrl'
+      }).
+      state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
       });
 
     $urlRouterProvider.otherwise('home');
@@ -40,10 +45,32 @@ app.controller('MainCtrl',['$scope', 'posts', function($scope, posts){
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
       $scope.link = '';
       $scope.title = '';
+    };
+  }
+]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+    $scope.addComment = function(){
+      if($scope.body === ''){ return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
     };
   }
 ]);
